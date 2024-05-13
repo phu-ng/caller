fe8b85bf56194bd31fce8fc4ef3ca97ea3ed2bcc
Phu Nguyen <phunguyen9297@gmail.com>
2024-05-13 14:07:07 +0700

    add bad code

--- src/main/java/com/example/caller/CallController.java
+++ src/main/java/com/example/caller/CallController.java
@@ -10,6 +10,17 @@ public class CallController {
     @Autowired
     private Environment env;
 
+    public void doSomething(int a, int b) {
+        if (a == b)
+            System.out.println("a equals b");
+        else if (a != b)
+            System.out.println("a does not equal b");
+        else if (a < b)
+            System.out.println("a is less than b");
+        else if (a > b)
+            System.out.println("a is greater than b");
+    }
+
     @GetMapping("/call")
     public String call() {
         return env.getProperty("service.b");
